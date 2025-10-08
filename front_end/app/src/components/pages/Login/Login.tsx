e534d40976c5491afdc02891f60eb0a521e8ec50
Rhode01 <Rhodrickmhone@gmail.com>
2025-10-08 15:47:01 +0200

    .

---
+++ front_end/app/src/components/pages/Login/Login.tsx
@@ -0,0 +1,40 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useEffect, useState } from 'react'
+import { redirect } from 'react-router-dom'
+export const Login = () => {
+    const [loginDetails, setLoginDetails]  = useState({
+      username:"",
+      password:""
+    }) 
+    const {isAuthenticated,loginWithRedirect } = useAuth0()
+    useEffect(()=>{
+        if (isAuthenticated){
+           redirect("/dashboard")
+        }    
+    })
+    const handleFormSubmission = (event) =>{
+      event.preventDefault();
+    }
+    const onChangeFormData = (e) => {
+        const { name, value } = e.target;
+        setLoginDetails((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+      };
+
+    }
+  return (
+    <div>
+      <div>
+          <form>
+            <input type="text" name="username" 
+             onChange={onChangeFormData}
+            placeholder='Enter your registration Number'/>
+            <input type="password" name="password" id="" placeholder='Enter password' />
+            <input type="submit" value="Login" />
+       </form>
+      </div>
+    </div>
+  )
+}
